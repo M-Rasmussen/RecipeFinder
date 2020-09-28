0351c62614760fee0c229c0c73c37d4aa2b57220
EC2 Default User <ec2-user@ip-172-31-39-121.ec2.internal>
2020-09-28 15:47:48 +0000

    created more random in the search for recipe, now returns different if the same text is entered. Also made it so just refreshing the site will give a new random recipe, solved the fact of blank screen when first pulled up site.

--- project1.py
+++ project1.py
@@ -96,6 +96,7 @@ app = flask.Flask(__name__)
 
 @app.route('/',methods=['post','get'])
 def searchfood():
+    '''Variables being that will store information to pass through the flask API'''
     nameSP=""
     picSP=""
     linkSP=""
@@ -106,61 +107,74 @@ def searchfood():
     tweetText="NONE"
     tweetUserName="NONE"
     tweetDate="NONE"
+    a=""
+    '''IF the user fills out the form'''
     if request.method=='POST':
         a=request.form.get('search')
-        url="https://api.spoonacular.com/recipes/complexSearch?query="+a+"&number=1&apiKey={}".format(spoonacular_key)
-        
+        url="https://api.spoonacular.com/recipes/complexSearch?query="+a+"&number=10&apiKey={}".format(spoonacular_key)
         response = requests.get(url)
         json_body = response.json()
-        '''Variables being that will store information to pass through the flask API'''
+        
+        '''get reandom number so if one puts in the same recipe a different one will show up'''
+        ranNum=random.randint(0,9)
+        foodId=(json.dumps(json_body["results"][ranNum]["id"],indent=2))
+        
+    else:
+        '''if the user does not fill out the form, but refreshes the page and wants to get a random recipe''' 
+        url="https://api.spoonacular.com/recipes/random?number=1&apiKey={}".format(spoonacular_key)
+        response = requests.get(url)
+        json_body = response.json()
+        foodId=(json.dumps(json_body["recipes"][0]["id"],indent=2))
 
+    ''' USE ID gathered from foodID API and then parse the information from the API and store it in variables declared above'''
+    url2 = "https://api.spoonacular.com/recipes/"+foodId+"/information?apiKey={}".format(spoonacular_key)
+
+    response2 = requests.get(url2)
+    json_body2 = response2.json()
+    print("**************************")
+    print(json.dumps(json_body2, indent=2))
+    print("**************************")    
+    for item in json_body2["extendedIngredients"]:
+        ingredientsSP.append(item["original"]) 
         
-        foodId=(json.dumps(json_body["results"][0]["id"],indent=2))
-        
-        ''' USE ID gathered from foodID API and then parse the information from the API and store it in variables declared above'''
-        url2 = "https://api.spoonacular.com/recipes/"+foodId+"/information?apiKey={}".format(spoonacular_key)
-        response2 = requests.get(url2)
-        json_body2 = response2.json()
-        '''print(json.dumps(json_body2, indent=2))'''
-        for item in json_body2["extendedIngredients"]:
-            ingredientsSP.append(item["original"]) 
+    for instructOut in json_body2["analyzedInstructions"]:
+        for instructIn in instructOut["steps"]:
+            instructionsSP.append(instructIn["step"])
             
-        for instructOut in json_body2["analyzedInstructions"]:
-            for instructIn in instructOut["steps"]:
-                instructionsSP.append(instructIn["step"])
-                
-        nameSP=json_body2["title"]
-        picSP=json_body2["image"]
-        linkSP=json_body2["spoonacularSourceUrl"]
-        servingSizeSP=json_body2["servings"]
-        prepTimeSP=json_body2["readyInMinutes"]
-        
-        '''make the title of recipe a hashtag'''
-        tweetnameSearch=nameSP.replace(" ","")
-       
-        
-        print("*******************")
-        print(nameSP)
-        print(picSP)
-        print(linkSP)
-        print(servingSizeSP)
-        for x in ingredientsSP:
-            print(x)
-        for y in instructionsSP:
-            print(y)
-        print(tweetnameSearch)
-        
-        tweets=tweepy.Cursor(auth_api.search, q=tweetnameSearch, lang="en",since=date_since).items(1)
+    nameSP=json_body2["title"]
+    picSP=json_body2["image"]
+    linkSP=json_body2["spoonacularSourceUrl"]
+    servingSizeSP=json_body2["servings"]
+    prepTimeSP=json_body2["readyInMinutes"]
+    
+    '''make the title of recipe a hashtag'''
+    tweetnameSearch=nameSP.replace(" ","")
+   
+    
+    print("*******************")
+    print(nameSP)
+    print(picSP)
+    print(linkSP)
+    print(servingSizeSP)
+    for x in ingredientsSP:
+        print(x)
+    for y in instructionsSP:
+        print(y)
+    print(tweetnameSearch)
+    
+    tweets=tweepy.Cursor(auth_api.search, q=tweetnameSearch, lang="en",since=date_since).items(1)
+    for tweet in tweets:
+        tweetText=tweet.text
+        tweetUserName=tweet.user.screen_name
+        tweetDate=tweet.created_at
+    print(tweetText)
+    print(a)
+    if tweetText=='NONE' and a !="":
+        tweets=tweepy.Cursor(auth_api.search, q=a, lang="en",since=date_since).items(1)
         for tweet in tweets:
             tweetText=tweet.text
             tweetUserName=tweet.user.screen_name
             tweetDate=tweet.created_at
-        if tweetText=='NONE':
-            tweets=tweepy.Cursor(auth_api.search, q=a, lang="en",since=date_since).items(1)
-            for tweet in tweets:
-                tweetText=tweet.text
-                tweetUserName=tweet.user.screen_name
-                tweetDate=tweet.created_at
     return flask.render_template("index.html", iName = nameSP, iPic = picSP, iServing=servingSizeSP, iMinutes=prepTimeSP, iIngredients=ingredientsSP, iInstructions=instructionsSP, itweeterName=tweetUserName, itweeterText=tweetText, itweeterDate=tweetDate, iLink= linkSP)
 
 
